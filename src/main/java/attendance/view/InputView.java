2332114cb4bb3529a7592aac5c84cd196b506d58
snow___shower <wonsb25@gmail.com>
2026-01-08 17:41:31 +0900

    feat: 입출력 양식 작성

---
+++ src/main/java/attendance/view/InputView.java
@@ -0,0 +1,38 @@
+package attendance.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    //기능선택
+    public String inputFunction(){
+        return Console.readLine();
+    }
+
+    //출석확인, 출석기록확인
+    public String inputNickname(){
+        System.out.println("닉네임을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String inputStartTime(){
+        System.out.println("등교 시간을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    //출석수정
+    public String inputUpdateNickname(){
+        System.out.println("출석을 수정하려는 크루의 닉네임을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String inputUpdateDate(){
+        System.out.println("수정하려는 날짜(일)를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public String inputUpdateTime(){
+        System.out.println("언제로 변경하겠습니까?");
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/attendance/view/OutputView.java
@@ -0,0 +1,29 @@
+package attendance.view;
+
+public class OutputView {
+
+    public void outputFunction(){
+        System.out.println("오늘은 월 일 금요일입니다. 기능을 선택해 주세요.\n" +
+                "1. 출석 확인\n" +
+                "2. 출석 수정\n" +
+                "3. 크루별 출석 기록 확인\n" +
+                "4. 제적 위험자 확인\n" +
+                "Q. 종료\n");
+    }
+
+    //출석확인
+    public void outputAttendance(){
+        System.out.println();
+    }
+
+    //출석수정
+    public void outputUpdateInfo(){
+        System.out.println();
+    }
+
+    //출석기록확인
+    public void outputRecord(){
+        System.out.println();
+    }
+
+}
