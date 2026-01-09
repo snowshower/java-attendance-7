bfe0a99e486ee93be760f048ff41c5f614b048ff
snow___shower <wonsb25@gmail.com>
2026-01-09 14:38:36 +0900

    feat: 컨트롤러 작성

--- src/main/java/attendance/controller/AttendanceController.java
+++ src/main/java/attendance/controller/AttendanceController.java
@@ -4,6 +4,9 @@ import attendance.service.AttendanceService;
 import attendance.view.InputView;
 import attendance.view.OutputView;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class AttendanceController {
     private final InputView inputView;
     private final OutputView outputView;
@@ -16,6 +19,36 @@ public class AttendanceController {
     }
 
     public void run(){
+        while(true){
+            outputView.outputFunction();
+            String s=inputView.inputFunction();
+            LocalDate today=LocalDate.of(2024,12,13);
 
+            //출석확인
+            if(s.equals("1")){
+                String nickname=inputView.inputNickname();
+                LocalTime time=LocalTime.parse(inputView.inputStartTime());
+                attendanceService.checkAttendance(nickname, today, time);
+            }
+
+            //출석수정
+            if(s.equals("2")){
+                String nickname=inputView.inputNickname();
+                int date= inputView.inputUpdateDate();
+                LocalTime time=LocalTime.parse(inputView.inputUpdateTime());
+                attendanceService.updateAttendance(nickname, date, time);
+            }
+
+            //출석기록확인
+            if(s.equals("3")){
+                String nickname=inputView.inputNickname();
+                outputView.outputRecord(attendanceService.formatAttendanceRecord(nickname));
+            }
+
+            //종료
+            if(s.equals("Q")){
+                break;
+            }
+        }
     }
 }
